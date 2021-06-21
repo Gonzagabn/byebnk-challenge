1493b4ea787fc0881903a697bf8e6b2c9bb38a78
Gonzaga <luizgonzagabn@gmail.com>
2021-06-21 17:09:35 -0300

    commit

--- lib/screens/transactions_and_balance_screen.dart
+++ lib/screens/transactions_and_balance_screen.dart
@@ -1,10 +1,7 @@
-import 'package:byebnk_app/utils/app_routes.dart';
 import 'package:byebnk_app/widgets/app_drawer.dart';
 import 'package:byebnk_app/widgets/balance_card.dart';
+import 'package:byebnk_app/widgets/transactions_card.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:byebnk_app/providers/transactions.dart';
-import 'package:byebnk_app/widgets/transaction_item.dart';
 
 class TransactionsAndBalanceScreen extends StatelessWidget {
   const TransactionsAndBalanceScreen({Key? key}) : super(key: key);
@@ -27,7 +24,9 @@ class TransactionsAndBalanceScreen extends StatelessWidget {
           Container(
             color: Colors.black,
             height: availableHeight * 0.2,
-            child: Center(child: BalanceCard()),
+            child: Center(
+              child: BalanceCard(),
+            ),
           ),
           Container(
             color: Colors.black,
@@ -59,47 +58,7 @@ class TransactionsAndBalanceScreen extends StatelessWidget {
                     ),
                     height: availableHeight * 0.75,
                     width: availableWidth * 0.9,
-                    child: FutureBuilder(
-                      future: Provider.of<Transactions>(context, listen: false)
-                          .loadTransactions(),
-                      builder: (ctx, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return Center(
-                            child:
-                                CircularProgressIndicator(color: Colors.black),
-                          );
-                        } else if (snapshot.hasError) {
-                          return AlertDialog(
-                            title: Text('Ocorreu um erro!'),
-                            content: Text('Ocorreu um erro FAKE!'),
-                            actions: <Widget>[
-                              TextButton(
-                                child: Text('Fechar'),
-                                onPressed: () {
-                                  Navigator.of(context)
-                                      .pushReplacementNamed(AppRoutes.TXNS_BAL);
-                                },
-                              )
-                            ],
-                          );
-                        } else {
-                          return Consumer<Transactions>(
-                            builder: (ctx, txns, cild) {
-                              return ListView.builder(
-                                itemCount: txns.transactionsCount,
-                                itemBuilder: (ctx, index) => Column(
-                                  children: <Widget>[
-                                    TransactionItem(txns.transactions[index]),
-                                    Divider(),
-                                  ],
-                                ),
-                              );
-                            },
-                          );
-                        }
-                      },
-                    ),
+                    child: TransactionsCard(),
                   ),
                 ),
               ],
---
+++ lib/widgets/transactions_card.dart
@@ -0,0 +1,54 @@
+import 'package:byebnk_app/providers/transactions.dart';
+import 'package:byebnk_app/utils/app_routes.dart';
+import 'package:byebnk_app/widgets/transaction_item.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class TransactionsCard extends StatelessWidget {
+  const TransactionsCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future:
+          Provider.of<Transactions>(context, listen: false).loadTransactions(),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(color: Colors.black),
+          );
+        } else if (snapshot.hasError) {
+          return AlertDialog(
+            title: Text('Ocorreu um erro!'),
+            content: Text('Ocorreu um erro FAKE!'),
+            actions: <Widget>[
+              TextButton(
+                child: Text('Fechar'),
+                onPressed: () {
+                  Navigator.of(context)
+                      .pushReplacementNamed(AppRoutes.TXNS_BAL);
+                },
+              )
+            ],
+          );
+        } else {
+          return Consumer<Transactions>(
+            builder: (ctx, txns, cild) {
+              return ListView.builder(
+                itemCount: txns.transactionsCount,
+                itemBuilder: (ctx, index) => Column(
+                  children: <Widget>[
+                    TransactionItem(txns.transactions[index]),
+                    Divider(),
+                  ],
+                ),
+              );
+            },
+          );
+        }
+      },
+    );
+  }
+}
