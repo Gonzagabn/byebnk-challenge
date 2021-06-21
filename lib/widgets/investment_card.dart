7494f1326200a2cf401fa408c6e2df46cdc81300
Gonzaga <luizgonzagabn@gmail.com>
2021-06-21 19:51:55 -0300

    commit

--- lib/widgets/investment_card.dart
+++ lib/widgets/investment_card.dart
@@ -1,6 +1,5 @@
 import 'package:byebnk_app/exceptions/http_exceptions.dart';
 import 'package:byebnk_app/providers/investment.dart';
-import 'package:byebnk_app/providers/transactions.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,7 +13,7 @@ class InvestmentCard extends StatefulWidget {
 class _InvestmentCard extends State<InvestmentCard> {
   GlobalKey<FormState> _form = GlobalKey();
   bool _isLoading = false;
-  double? _invData;
+  Investment _invsData = Investment(date: DateTime.now(), value: 0.00);
 
   void _showErrorDialog(String msg) {
     showDialog(
@@ -48,7 +47,7 @@ class _InvestmentCard extends State<InvestmentCard> {
     Investments invs = Provider.of(context, listen: false);
 
     try {
-      await invs.investmentRequest(_invData!);
+      await invs.investmentRequest(_invsData.value);
     } on HttpException catch (error) {
       _showErrorDialog(error.toString());
     } catch (error) {
@@ -106,67 +105,64 @@ class _InvestmentCard extends State<InvestmentCard> {
                     ),
                   ),
                 ),
-                Consumer<Transactions>(
-                  builder: (ctx, txns, cild) {
-                    return Form(
-                      key: _form,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Center(
-                            child: Container(
-                              width: availableWidth * 0.7,
-                              child: TextFormField(
-                                decoration: InputDecoration(
-                                  labelText:
-                                      'Informe o valor a ser aplicado...',
-                                ),
-                                keyboardType: TextInputType.numberWithOptions(
-                                    decimal: true),
-                                validator: (value) {
-                                  if (value!.isEmpty ||
-                                      double.parse(value) == 0.0) {
-                                    return 'Informe um valor válido!';
-                                  }
-                                  return null;
-                                },
-                                onSaved: (value) =>
-                                    _invData = double.parse(value!),
-                              ),
+                Form(
+                  key: _form,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Center(
+                        child: Container(
+                          width: availableWidth * 0.7,
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Informe o valor a ser aplicado...',
                             ),
+                            keyboardType:
+                                TextInputType.numberWithOptions(decimal: true),
+                            validator: (value) {
+                              if (value!.isEmpty ||
+                                  double.parse(value) == 0.0) {
+                                return 'Informe um valor válido!';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _invsData.value = double.parse(value!);
+                              _invsData.date = DateTime.now();
+                            },
                           ),
-                          SizedBox(
-                            height: availableHeight * 0.02,
-                          ),
-                          if (_isLoading)
-                            CircularProgressIndicator(
-                              color: Colors.black,
-                            )
-                          else
-                            ElevatedButton(
-                              style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all(Colors.black),
-                                shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(30),
-                                  ),
-                                ),
-                                padding: MaterialStateProperty.all(
-                                  EdgeInsets.symmetric(
-                                    horizontal: 30.0,
-                                    vertical: 8.0,
-                                  ),
-                                ),
-                              ),
-                              child: Text('APLICAR'),
-                              onPressed: _submit,
-                            ),
-                        ],
+                        ),
                       ),
-                    );
-                  },
-                ),
+                      SizedBox(
+                        height: availableHeight * 0.02,
+                      ),
+                      if (_isLoading)
+                        CircularProgressIndicator(
+                          color: Colors.black,
+                        )
+                      else
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.black),
+                            shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                            ),
+                            padding: MaterialStateProperty.all(
+                              EdgeInsets.symmetric(
+                                horizontal: 30.0,
+                                vertical: 8.0,
+                              ),
+                            ),
+                          ),
+                          child: Text('APLICAR'),
+                          onPressed: _submit,
+                        ),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
